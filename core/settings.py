923541aa78cb8d97bb399e63db115ad11fe92baa
vovinov <v.v.ovinov@yandex.ru>
2025-05-31 11:45:39 +0300

    Add docker postgres container to project

--- core/settings.py
+++ core/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("POSTGRES_DB"),
+        'USER': env("POSTGRES_USER"),
+        'PASSWORD': env("POSTGRES_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
