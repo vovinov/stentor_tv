cce23a0b791a262eb1aa41774ff5bef57f6186ca
vovinov <v.v.ovinov@yandex.ru>
2025-05-31 20:59:34 +0300

    Add first view and template

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from news.views import news_view
+
 urlpatterns = [
+    path('', news_view),
     path('admin/', admin.site.urls),
 ]
