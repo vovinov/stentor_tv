93ed3f783c6d2b74694d1d7262508be8baca6a5b
Овинов Вячеслав <vovinov@hqtv.rbc.ru>
2025-12-10 17:32:56 +0300

    Commit 101225/1 - add base routing

--- main.py
+++ main.py
@@ -1,6 +1,64 @@
-def main():
-    print("Hello from stentor-tv!")
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+class MessageCreate(BaseModel):
+    content: str
+
+class Message(BaseModel):
+    id: int
+    content: str
+
+
+# Инициализируем messages_db как список объектов Message
+messages_db: list[Message] = [Message(id=0, content="First post in FastAPI")]
+
+
+@app.get("/messages", response_model=list[Message])
+async def read_messages() -> list[Message]:
+    return messages_db
+
+
+@app.get("/messages/{message_id}", response_model=Message)
+async def read_message(message_id: int) -> Message:
+    for message in messages_db:
+        if message.id == message_id:
+            return message
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Message not found")
+
+
+@app.post("/messages", response_model=Message, status_code=status.HTTP_201_CREATED)
+async def create_message(message_create: MessageCreate) -> Message:
+    # Проверяем, что предоставленный ID уникален
+    next_id = max((msg.id for msg in messages_db), default=-1) + 1
+    new_message = Message(id=next_id, content=message_create.content)
+    messages_db.append(new_message)
+    return new_message
+
+
+@app.put("/messages/{message_id}", response_model=Message)
+async def update_message(message_id: int, message_create: MessageCreate) -> Message:
+    
+    for i, message in enumerate(messages_db):
+        if message.id == message_id:
+            updated_message = Message(id=message_id, content=message_create.content)
+            messages_db[i] = update_message
+            return updated_message
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Сообщение не найдено")
+
+
+@app.delete("/messages/{message_id}", status_code=status.HTTP_200_OK)
+async def delete_message(message_id: int) -> dict:
+    for i, message in enumerate(messages_db):
+        if message.id == message_id:
+            messages_db.pop(i)
+            return {"detail": f"Message ID={message_id} deleted!"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Message not found")
+
+
+@app.delete("/messages", status_code=status.HTTP_200_OK)
+async def delete_messages() -> dict:
+    messages_db.clear()
+    return {"detail": "All messages deleted!"}
